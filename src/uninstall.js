20aa523e9cb232f85df59fd719d7276bafc2002a
Vagner Pene <vagnerpene@gmail.com>
2021-03-11 11:10:40 +0200

    Update file path

--- src/uninstall.js
+++ src/uninstall.js
@@ -7,8 +7,6 @@ var svc = new Service({
   script: path.join(__dirname, "server.js"),
 });
 
-console.log(path.join(__dirname, "server.js"))
-
 // Listen for the "uninstall" event so we know when it's done.
 svc.on("uninstall", function () {
   console.log("Uninstall complete.");
